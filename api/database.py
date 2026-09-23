d10c37d246faa00a79532dd60108d57a56329ec8
Krishna <ckrishna122517@gmail.com>
2026-09-23 23:30:51 +0800

    Fix database connection for deployment

--- api/database.py
+++ api/database.py
@@ -1,13 +1,17 @@
+
+import os
 import mysql.connector
 
 def get_db_connection():
     connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="krishnasree123@",
-        database="skill_bridge"
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT", "3306")),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
     return connection
+    
 
 
 if __name__ == "__main__":
